f3cfd2ca76c0179ded6b7542df51c09f3b114e5f
Zoe Cope <zoe.cope@hmcts.net>
2024-10-24 18:12:22 +0100

    correction

--- containers.tf
+++ containers.tf
@@ -1,35 +1,49 @@
 resource "azurerm_cosmosdb_sql_container" "this" {
-  for_each = {
-    for db_key, db_value in var.databases :
-    db_key => db_value.containers
-  }
+  for_each = var.containers
 
-  name                = each.value.container_name
-  resource_group_name = var.resource_group_name
-  account_name        = azurerm_cosmosdb_account.this.name
-  database_name       = each.value.database_name
-  partition_key_paths = each.value.partition_key_paths
-  throughput          = each.value.throughput
-
-  indexing_policy {
-    indexing_mode = each.value.indexing_mode
-
-    dynamic "included_path" {
-      for_each = each.value.included_path
-      content {
-        path = included_path.value
-      }
-    }
-
-    dynamic "excluded_path" {
-      for_each = each.value.excluded_path
-      content {
-        path = excluded_path.value
-      }
+  name                   = each.value.container_name
+  resource_group_name    = var.resource_group_name
+  account_name           = azurerm_cosmosdb_account.this.name
+  database_name          = each.value.db_name
+  partition_key_path     = each.value.partition_key_path
+  partition_key_version  = each.value.partition_key_version != null ? each.value.partition_key_version : 2
+  throughput             = each.value.container_max_throughput != null ? null : each.value.container_throughout
+  default_ttl            = each.value.default_ttl != null ? each.value.default_ttl : null
+  analytical_storage_ttl = each.value.analytical_storage_ttl != null ? each.value.analytical_storage_ttl : null
+  dynamic "autoscale_settings" {
+    for_each = each.value.container_max_throughput != null ? [1] : []
+    content {
+      max_throughput = each.value.container_max_throughput
     }
   }
 
-  unique_key {
-    paths = each.value.unique_key_paths
+  dynamic "indexing_policy" {
+    for_each = length(each.value.indexing_policy_settings) > 0 ? [1] : []
+    content {
+      indexing_mode = each.value.indexing_policy_settings.sql_indexing_mode != null ? each.value.indexing_policy_settings.sql_indexing_mode : null
+
+      dynamic "included_path" {
+        for_each = each.value.indexing_policy_settings.sql_included_path != null ? [1] : []
+        content {
+          path = each.value.indexing_policy_settings.sql_included_path
+        }
+      }
+      dynamic "excluded_path" {
+        for_each = each.value.indexing_policy_settings.sql_excluded_path != null ? [1] : []
+        content {
+          path = each.value.indexing_policy_settings.sql_excluded_path
+        }
+      }
+    }
   }
+  dynamic "unique_key" {
+    for_each = length(each.value.sql_unique_key) > 0 ? each.value.sql_unique_key : []
+    content {
+      paths = each.value.sql_unique_key
+    }
+  }
+
+  depends_on = [
+    azurerm_cosmosdb_sql_database.this
+  ]
 }
--- database.tf
+++ database.tf
@@ -1,10 +1,14 @@
 resource "azurerm_cosmosdb_sql_database" "this" {
-  for_each = {
-    for index, db in var.databases :
-    db.name => db
-  }
-
-  name                = each.value.database_name
+  for_each            = var.databases
+  name                = each.value.db_name
   resource_group_name = var.resource_group_name
   account_name        = azurerm_cosmosdb_account.this.name
+  throughput          = each.value.db_max_throughput != null ? null : each.value.db_throughput
+
+  dynamic "autoscale_settings" {
+    for_each = each.value.db_max_throughput != null ? [1] : []
+    content {
+      max_throughput = each.value.db_max_throughput
+    }
+  }
 }
