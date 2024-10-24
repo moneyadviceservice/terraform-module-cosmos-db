de756d363e9c5ac1b95a475b820da97a29e12aea
Zoe Cope <zoe.cope@hmcts.net>
2024-10-24 17:19:48 +0100

    map arguments

--- containers.tf
+++ containers.tf
@@ -1,36 +1,34 @@
-
 resource "azurerm_cosmosdb_sql_container" "this" {
-  for_each              = { for db_key, db_value in var.databases : db_key => db_value }
-  name                  = each.value.containers[container_name].container_name
+  for_each = {
+    for db_key, db_value in var.databases :
+  db_key => db_value.containers }
+  name                  = each.value.container_name
   resource_group_name   = var.resource_group_name
   account_name          = azurerm_cosmosdb_account.this.name
-  database_name         = azurerm_cosmosdb_sql_database.this[each.key].name
-  partition_key_paths   = each.value.containers[container_name].partition_key_paths
+  database_name         = azurerm_cosmosdb_sql_database[each.key].name
+  partition_key_paths   = each.value.partition_key_paths
   partition_key_version = 1
-  throughput            = each.value.containers[container_name].throughput
+  throughput            = each.value.throughput
 
   indexing_policy {
-    indexing_mode = each.value.containers[container_name].indexing_mode
+    indexing_mode = each.value.indexing_mode
 
     dynamic "included_path" {
-      for_each = each.value.containers[container_name].included_path
+      for_each = each.value.included_path
       content {
         path = included_path.value
       }
     }
 
     dynamic "excluded_path" {
-      for_each = each.value.containers[container_name].excluded_path
+      for_each = each.value.excluded_path
       content {
         path = excluded_path.value
       }
     }
   }
 
-  dynamic "unique_key" {
-    for_each = each.value.containers[container_name].unique_key_paths
-    content {
-      paths = [unique_key.value]
-    }
+  unique_key {
+    paths = each.value.unique_key_paths
   }
 }
--- database.tf
+++ database.tf
@@ -1,6 +1,9 @@
 resource "azurerm_cosmosdb_sql_database" "this" {
-  for_each = var.databases
-
+  for_each = {
+    for index, db in var.databases :
+    db.name => db
+  }
+  # count = length(var.db_names)
   name                = each.value.database_name
   resource_group_name = var.resource_group_name
   account_name        = azurerm_cosmosdb_account.this.name
